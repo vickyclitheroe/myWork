222f40d21cec0d9b19ac6337700a6271acd9d585
Vicky <vickyclitheroe@gmail.com>
2014-04-15 11:06:00 +0100

    Create buildASnowman.js

---
+++ buildASnowman.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+$("#head").draggable();
+$("#body").draggable();
+$("#nose").draggable();
+$("#eye1").draggable();
+$("#eye2").draggable();
+$("#mouth").draggable();
+
+$("#button1").draggable();
+$("#button2").draggable();
+$("#button3").draggable();
+
+$("#snow").effect("drop", "slow");
+});
