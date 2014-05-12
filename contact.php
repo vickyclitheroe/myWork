6e01ba5370e7733c116717215d15e638844532eb
Vicky <vickyclitheroe@gmail.com>
2014-05-12 13:13:22 +0100

    Removed old files

--- contact.php
+++
@@ -1,9 +0,0 @@
-<?php
-  $Name= $_REQUEST['Name'] ;
-  $EmailAddress = $_REQUEST['Email Address'] ;
-  $message = $_REQUEST['message'] ;
-
-  mail( "vickyclitheroe@gmail.com", "Feedback Form Results",
-    $message, "From: $email" );
-  header( "Location: http://www.example.com/thankyou.html" );
-?>
