4fe5c81103c6a4afc72962e9002c71ef23d709c3
Vicky <vickyclitheroe@gmail.com>
2014-05-25 18:30:18 +0100

    Create 8ball.js

---
+++ 8ball.js
@@ -0,0 +1,90 @@
+#h1{
+  left:40%;
+  font-family:Geneva,tahoma;
+  top:15%;
+  position:absolute;
+  color:black;
+  
+}
+#body{
+  background-color:#350FA8;
+}
+
+#Message
+{
+  color : #1B2DF2;
+  position : absolute;
+  top : 40%;
+  left : 36%;
+  word-wrap: break-word;
+   width: 80px;
+  text-align:center;
+  text-shadow:5px 5px 30px #4E75F5;
+  font-size:14px;
+ 
+}
+
+#ballParent{
+  position:relative;
+  left : 25%;
+  margin-top : 15%;
+  
+}
+
+#ball{
+  background-color:black;
+  height:500px;
+  width:500px;
+  border-radius:500px;
+  position:absolute;
+ top:0%;
+  left:5%;
+  
+  
+}
+
+#circle{
+  background-color:white;
+  height:300px;
+  width:300px;
+  position : absolute;
+  border-radius:300px;
+  margin-top:15%;
+  margin-left:20%
+  
+ 
+}
+
+#triangle{
+   width: 0; 
+	height: 0; 
+	border-left: 100px solid transparent;
+	border-right: 100px solid transparent;
+	
+	border-top: 140px solid #07091F;
+  position:absolute;
+  margin-left:17%;
+  margin-top:30%;
+
+}
+
+#textBox{
+  
+ border-radius:20%;
+  
+  
+}
+
+#button{
+  
+  border-radius:20%;
+  border:black;
+  background-color:grey;
+  
+}
+
+#form{
+  position:absolute;
+  left:40%;
+  margin-top : 40%;
+}
