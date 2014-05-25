bcccb5fec282b5b154034f6ce03611f5c11703b0
Vicky <vickyclitheroe@gmail.com>
2014-05-25 18:54:51 +0100

    Update 8ball.js

--- 8ball.js
+++ 8ball.js
@@ -1,90 +1,67 @@
-#h1{
-  left:40%;
-  font-family:Geneva,tahoma;
-  top:15%;
-  position:absolute;
-  color:black;
+$(document).ready(function() {
   
-}
-#body{
-  background-color:#350FA8;
-}
+  var answers = [ "It is certain",
+"It is decidedly so",
+ "Without a doubt",
+ "Yes definitely",
+ "You may rely on it",
+ "As I see it, yes",
+ "Most likely",
+ "Outlook good",
+ "Yes",
+ "Signs point to yes",
+ "Reply hazy try again",
+ "Ask again later",
+ "Better not tell you now",
+ "Cannot predict now",
+ "Concentrate and ask again",
+ "Don't count on it",
+ "My reply is no",
+"My sources say no",
+ "Outlook not so good",
+ "Very doubtful"];
+    
+    
 
-#Message
+
+$('#button').click(function(){
+  
+  $('#ball').effect('shake');
+  var question = $('#textBox').val();
+  
+  if ( question.length <= 2)
+  {
+     showTextInTriangle( "You need to enter a real question ");
+  }
+  else
+  {
+     showAnswer(); 
+  }
+
+});
+
+
+
+function showAnswer()
 {
-  color : #1B2DF2;
-  position : absolute;
-  top : 40%;
-  left : 36%;
-  word-wrap: break-word;
-   width: 80px;
-  text-align:center;
-  text-shadow:5px 5px 30px #4E75F5;
-  font-size:14px;
+    var randomNumber = Math.floor(Math.random()*answers.length)
+   
+    var answer = answers[randomNumber];
+   
+    showTextInTriangle( answer);
+}
  
-}
-
-#ballParent{
-  position:relative;
-  left : 25%;
-  margin-top : 15%;
+function showTextInTriangle( message )
+{
+  $("p#Message").text(message);
+  $("#Message").hide();
+  $("#Message").fadeIn(1500);
   
 }
 
-#ball{
-  background-color:black;
-  height:500px;
-  width:500px;
-  border-radius:500px;
-  position:absolute;
- top:0%;
-  left:5%;
-  
-  
-}
+$("#form").submit(function(e) {
+    e.preventDefault();
+});
 
-#circle{
-  background-color:white;
-  height:300px;
-  width:300px;
-  position : absolute;
-  border-radius:300px;
-  margin-top:15%;
-  margin-left:20%
-  
- 
-}
-
-#triangle{
-   width: 0; 
-	height: 0; 
-	border-left: 100px solid transparent;
-	border-right: 100px solid transparent;
-	
-	border-top: 140px solid #07091F;
-  position:absolute;
-  margin-left:17%;
-  margin-top:30%;
-
-}
-
-#textBox{
-  
- border-radius:20%;
-  
-  
-}
-
-#button{
-  
-  border-radius:20%;
-  border:black;
-  background-color:grey;
-  
-}
-
-#form{
-  position:absolute;
-  left:40%;
-  margin-top : 40%;
-}
+});
+    
